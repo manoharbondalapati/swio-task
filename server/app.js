40a875e20cb5ca23d12d618feadbfb09cfe1d42d
Manohar34 <138869883+manoharbondalapati@users.noreply.github.com>
2024-05-29 15:02:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -52,8 +52,8 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         }],
         mode:"payment",
         customer:customer.id,
-        success_url:"http://localhost:3000/success",
-        cancel_url:"http://localhost:3000/cancel",
+        success_url:"https://swio-task-seven.vercel.app/success",
+        cancel_url:"https://swio-task-seven.vercel.app/cancel",
     });
     //console.log(session.id)
     const sou={
